6d1ab4fce0780dd373fec078c27759f3322db1e4
Layora-cpu <layorahijabs@gmail.com>
2026-09-05 11:00:40 +0530

    Add product list to products.js

--- products.js
+++ products.js
@@ -1,1 +1,731 @@
+const products = [
 
+  {
+    id: "LAY001",
+    name: "Animal Print Hijab",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY001-1.jpg",
+    images: [
+      "assets/products/LAY001-1.jpg",
+      "assets/products/LAY001-2.jpg",
+      "assets/products/LAY001-3.jpg",
+      "assets/products/LAY001-4.jpg",
+      "assets/products/LAY001-5.jpg",
+      "assets/products/LAY001-6.jpg",
+      "assets/products/LAY001-7.jpg",
+      "assets/products/LAY001-8.jpg",
+      "assets/products/LAY001-9.jpg"
+    ],
+    badge: "BEST SELLER"
+  },
+
+  {
+    id: "LAY002",
+    name: "Bandana",
+    category: "Accessories",
+    mrp: 269,
+    price: 269,
+    image: "assets/products/LAY002-1.png",
+    images: [
+      "assets/products/LAY002-1.png",
+      "assets/products/LAY002-2.png"
+    ],
+    badge: ""
+  },
+
+  {
+    id: "LAY003",
+    name: "Bridal Georgette",
+    category: "Hijab",
+    mrp: 340,
+    price: 340,
+    image: "assets/products/LAY003-1.png",
+    images: [
+      "assets/products/LAY003-1.png",
+      "assets/products/LAY003-2.png"
+    ],
+    badge: "PREMIUM"
+  },
+
+  {
+    id: "LAY004",
+    name: "Bubble Satin Stone",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY004-1.png",
+    images: [
+      "assets/products/LAY004-1.png",
+      "assets/products/LAY004-2.png"
+    ],
+    badge: ""
+  },
+
+  {
+    id: "LAY005",
+    name: "Cotton Hijab",
+    category: "Hijab",
+    mrp: 200,
+    price: 200,
+    image: "assets/products/LAY005-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY006",
+    name: "Cotton Black & White Hijab",
+    category: "Hijab",
+    mrp: 140,
+    price: 140,
+    image: "assets/products/LAY006-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY007",
+    name: "Cotton Print Hijab",
+    category: "Hijab",
+    mrp: 259,
+    price: 259,
+    image: "assets/products/LAY007-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY008",
+    name: "Digital Print Hijab",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY008-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY009",
+    name: "Edgem Embroidery",
+    category: "Hijab",
+    mrp: 349,
+    price: 349,
+    image: "assets/products/LAY009-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY010",
+    name: "Embroidery Organza",
+    category: "Hijab",
+    mrp: 329,
+    price: 329,
+    image: "assets/products/LAY010-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY011",
+    name: "Floral Print Cotton Hijab",
+    category: "Hijab",
+    mrp: 140,
+    price: 140,
+    image: "assets/products/LAY011-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY012",
+    name: "Hesh Nine Black Hijab",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY012-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY013",
+    name: "Heavy Chiffon Crochet",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY013-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY014",
+    name: "Heavy Chiffon",
+    category: "Hijab",
+    mrp: 200,
+    price: 200,
+    image: "assets/products/LAY014-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY015",
+    name: "Jersey Jersey",
+    category: "Hijab",
+    mrp: 289,
+    price: 289,
+    image: "assets/products/LAY015-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY016",
+    name: "Lux Organza",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY016-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY017",
+    name: "Malaysian Cotton",
+    category: "Hijab",
+    mrp: 269,
+    price: 269,
+    image: "assets/products/LAY017-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY018",
+    name: "Moja Hand Sleeves",
+    category: "Accessories",
+    mrp: 169,
+    price: 169,
+    image: "assets/products/LAY018-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY019",
+    name: "Moja Jersey",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY019-1.jpg",
+    images: [
+      "assets/products/LAY019-1.jpg",
+      "assets/products/LAY019-2.jpg",
+      "assets/products/LAY019-3.jpg",
+      "assets/products/LAY019-4.jpg"
+    ],
+    badge: "BEST SELLER"
+  },
+
+  {
+    id: "LAY020",
+    name: "Moja Kids Jersey",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY020-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY021",
+    name: "Moja Kids Organza",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY021-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY022",
+    name: "Moja Loop Pin",
+    category: "Pins",
+    mrp: 149,
+    price: 149,
+    image: "assets/products/LAY022-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY023",
+    name: "Moja Magnetic Hijab",
+    category: "Hijab",
+    mrp: 369,
+    price: 369,
+    image: "assets/products/LAY023-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY024",
+    name: "Moja Magnetic Pin",
+    category: "Pins",
+    mrp: 120,
+    price: 120,
+    image: "assets/products/LAY024-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY025",
+    name: "Moja Malaysian Georgette",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY025-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY026",
+    name: "Moja Organza",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY026-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY027",
+    name: "Moja Shoulder Sleeves",
+    category: "Accessories",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY027-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY028",
+    name: "Moja Strips Chiffon",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY028-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY029",
+    name: "Moja Tie Cap",
+    category: "Accessories",
+    mrp: 149,
+    price: 149,
+    image: "assets/products/LAY029-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY030",
+    name: "Muna Satin Hijab",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY030-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY031",
+    name: "Niqab",
+    category: "Accessories",
+    mrp: 169,
+    price: 169,
+    image: "assets/products/LAY031-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY032",
+    name: "Ombre Chiffon",
+    category: "Hijab",
+    mrp: 269,
+    price: 269,
+    image: "assets/products/LAY032-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY033",
+    name: "Ombre Double Color",
+    category: "Hijab",
+    mrp: 249,
+    price: 249,
+    image: "assets/products/LAY033-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY034",
+    name: "Ombre Turkish Cotton",
+    category: "Hijab",
+    mrp: 269,
+    price: 269,
+    image: "assets/products/LAY034-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY035",
+    name: "Organic Cotton Prayer Dress",
+    category: "Accessories",
+    mrp: 569,
+    price: 569,
+    image: "assets/products/LAY035-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY036",
+    name: "Organza",
+    category: "Hijab",
+    mrp: 249,
+    price: 249,
+    image: "assets/products/LAY036-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY037",
+    name: "Organza Shimmer Rhinestone",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY037-1.jpg",
+    images: [],
+    badge: "PREMIUM"
+  },
+
+  {
+    id: "LAY038",
+    name: "Organza Yusra",
+    category: "Hijab",
+    mrp: 349,
+    price: 349,
+    image: "assets/products/LAY038-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY039",
+    name: "Pashmina Hijab",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY039-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY040",
+    name: "Ravka Heavy Chiffon",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY040-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY041",
+    name: "Organza",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY041-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY042",
+    name: "Scrunchies",
+    category: "Accessories",
+    mrp: 149,
+    price: 149,
+    image: "assets/products/LAY042-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY043",
+    name: "Sidra Cap",
+    category: "Caps",
+    mrp: 60,
+    price: 60,
+    image: "assets/products/LAY043-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY044",
+    name: "Shimmer Crush Satin",
+    category: "Hijab",
+    mrp: 349,
+    price: 349,
+    image: "assets/products/LAY044-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY045",
+    name: "Shining Print",
+    category: "Hijab",
+    mrp: 200,
+    price: 200,
+    image: "assets/products/LAY045-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY046",
+    name: "Textured Satin",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY046-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY047",
+    name: "Velvet Scrunchies",
+    category: "Accessories",
+    mrp: 159,
+    price: 159,
+    image: "assets/products/LAY047-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY048",
+    name: "Yusra Chiffon",
+    category: "Hijab",
+    mrp: 319,
+    price: 319,
+    image: "assets/products/LAY048-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY049",
+    name: "Yusra Georgette Strips",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY049-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY050",
+    name: "Zara Bonnet Cap",
+    category: "Caps",
+    mrp: 149,
+    price: 149,
+    image: "assets/products/LAY050-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY051",
+    name: "Zara Crinkled Cotton",
+    category: "Hijab",
+    mrp: 269,
+    price: 269,
+    image: "assets/products/LAY051-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY052",
+    name: "Zara Crushed Organza",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY052-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY053",
+    name: "Zara Glimmer Organza",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY053-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY054",
+    name: "Zara Hijab Tie Cap",
+    category: "Caps",
+    mrp: 169,
+    price: 169,
+    image: "assets/products/LAY054-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY055",
+    name: "Zara Lux Jersey",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY055-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY056",
+    name: "Zara Lux Satin",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY056-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY057",
+    name: "Zara Triple Black",
+    category: "Hijab",
+    mrp: 349,
+    price: 349,
+    image: "assets/products/LAY057-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY058",
+    name: "Zara Magnetic Pin",
+    category: "Pins",
+    mrp: 120,
+    price: 120,
+    image: "assets/products/LAY058-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY059",
+    name: "Zara Malaysian Georgette",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY059-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY060",
+    name: "Zara Neck Cover",
+    category: "Accessories",
+    mrp: 199,
+    price: 199,
+    image: "assets/products/LAY060-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY061",
+    name: "Zara Shaper",
+    category: "Accessories",
+    mrp: 119,
+    price: 119,
+    image: "assets/products/LAY061-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY062",
+    name: "Zara Shoulder Sleeve",
+    category: "Accessories",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY062-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY063",
+    name: "Zara Tube Cap",
+    category: "Accessories",
+    mrp: 149,
+    price: 149,
+    image: "assets/products/LAY063-1.jpg",
+    images: [],
+    badge: ""
+  },
+
+  {
+    id: "LAY064",
+    name: "Zara Watercolor",
+    category: "Hijab",
+    mrp: 299,
+    price: 299,
+    image: "assets/products/LAY064-1.jpg",
+    images: [],
+    badge: ""
+  }
+
+];
